89369c648278a1a43bda6b1f92c83dbdf821e84b
AntonHakansson <anton.hakansson98@gmail.com>
2021-03-22 05:10:18 +0100

    migrated security conf to separete file

--- modules/options.nix
+++ modules/options.nix
@@ -47,7 +47,6 @@ with lib.my;
         home = "/home/${name}";
         group = "users";
         uid = 1000;
-        initialPassword = "nixos";
       };
 
     # Install user packages to /etc/profiles instead. Necessary for
