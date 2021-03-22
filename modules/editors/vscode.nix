508f5d686edcd79fc2638c3ed68b726f052d1753
AntonHakansson <anton.hakansson98@gmail.com>
2021-03-22 04:19:52 +0100

    added vscode editor module

---
+++ modules/editors/vscode.nix
@@ -0,0 +1,21 @@
+{ config, options, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.editors.vscode;
+in {
+  options.modules.editors.vscode = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    user.packages = with pkgs; [
+      (unstable.vscode-with-extensions.override {
+        vscodeExtensions = with vscode-extensions; [
+          bbenoist.Nix
+          vscodevim.vim
+        ];
+      })
+    ];
+  };
+}
