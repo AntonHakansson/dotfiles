f5056d08fcc268ad6656b4bcaf86262f91080d09
AntonHakansson <anton.hakansson98@gmail.com>
2021-06-11 00:33:35 +0200

    wacom user script

--- modules/hardware/wacom.nix
+++ modules/hardware/wacom.nix
@@ -3,21 +3,22 @@
 with lib;
 with lib.my;
 let cfg = config.modules.hardware.wacom;
-in
-{
-  options.modules.hardware.wacom = {
-    enable = mkBoolOpt false;
-  };
+in {
+  options.modules.hardware.wacom = { enable = mkBoolOpt false; };
 
   config = mkIf cfg.enable {
     # For my intuos4 pro. Doesn't work for cintiqs.
     services.xserver.wacom.enable = true;
     # TODO Move this to udev
-    system.userActivationScripts.wacom = ''
-      # lock tablet to main display
-      if xinput list --id-only "Wacom Intuos BT M Pen" 2>&1 >/dev/null; then
-        xinput map-to-output $(xinput list --id-only "Wacom Intuos BT M Pen") DVI-I-1
-      fi
-    '';
+    user.packages = with pkgs;
+      [
+        (writeScriptBin "wacom" ''
+          #!${stdenv.shell}
+          # lock tablet to main display
+          if xinput list --id-only "Wacom Intuos BT M Pen stylus" 2>&1 >/dev/null; then
+            xinput map-to-output $(xinput list --id-only "Wacom Intuos BT M Pen stylus") DVI-I-1
+          fi
+        '')
+      ];
   };
 }
