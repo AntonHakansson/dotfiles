6375ca5b2ae825be4db3f39bbeeb2bd407becbce
AntonHakansson <anton.hakansson98@gmail.com>
2021-06-21 12:43:06 +0200

    pipewire migration

--- modules/hardware/bluetooth.nix
+++ modules/hardware/bluetooth.nix
@@ -25,14 +25,6 @@ in {
 
       user.packages = with pkgs; [ unstable.bluez-tools  ];
 
-      hardware.pulseaudio = {
-        # NixOS allows either a lightweight build (default) or full build of
-        # PulseAudio to be installed.  Only the full build has Bluetooth
-        # support, so it must be selected here.
-        package = pkgs.unstable.pulseaudioFull;
-        # Enable additional codecs
-        extraModules = [ pkgs.unstable.pulseaudio-modules-bt ];
-      };
       hardware.bluetooth.config = {
         General = { Enable = "Source,Sink,Media,Socket"; };
       };
