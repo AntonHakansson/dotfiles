85580df033fde62014dd3e1d28f8e3831e1eff0a
AntonHakansson <anton.hakansson98@gmail.com>
2021-07-02 14:59:43 +0200

    Fixed Anne Pro 2 disconnect issues

--- modules/hardware/annepro.nix
+++ modules/hardware/annepro.nix
@@ -4,14 +4,17 @@ with lib;
 with lib.my;
 let cfg = config.modules.hardware.annepro;
 in {
-  options.modules.hardware.annepro = {
-    enable = mkBoolOpt false;
-  };
+  options.modules.hardware.annepro = { enable = mkBoolOpt false; };
 
   config = mkIf cfg.enable {
-    user.packages = with pkgs; [
-      # obinskit depend on electron which is marked insecure
-      # unstable.obinskit
-    ];
+
+    # https://www.reddit.com/r/AnnePro/comments/gruzcb/anne_pro_2_linux_cant_type_after_inactivity/
+    boot.kernelParams = [ "usbhid.quirks=0x04D9:0xA292:0x00000400" ];
+
+    user.packages = with pkgs;
+      [
+        # obinskit depend on electron which is marked insecure
+        # unstable.obinskit
+      ];
   };
 }
