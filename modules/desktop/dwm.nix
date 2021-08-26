5a7f442d286551edf7ae4c35f5ede423f91b2ff6
AntonHakansson <anton.hakansson98@gmail.com>
2021-08-26 09:32:28 +0200

    dwmblocks: audio, filesystem modules, etc

--- modules/desktop/dwm.nix
+++ modules/desktop/dwm.nix
@@ -4,26 +4,52 @@ with lib;
 with lib.my;
 let
   cfg = config.modules.desktop.dwm;
-  memory = pkgs.writeScriptBin "sb-memory" ''
-    case $BLOCK_BUTTON in
-      1) ${pkgs.libnotify}/bin/notify-send "Memory hogs" "$(${pkgs.procps}/bin/ps axch -o cmd:15,%mem --sort=-%mem | ${pkgs.coreutils}/bin/head)" ;;
-      2) ${pkgs.util-linux}/bin/setsid -f "$TERMINAL" -e htop --sort PERCENT_MEM ;;
+  sb-audio = pkgs.writeScriptBin "sb-audio" ''
+    case $BUTTON in
+      1) ${pkgs.pavucontrol}/bin/pavucontrol --tab=3
+        ;;
+    esac
+    ${pkgs.alsa-utils}/bin/amixer -M sget Master | ${pkgs.gnugrep}/bin/grep -o -E [[:digit:]]+% | ${pkgs.coreutils}/bin/head -n 1
+  '';
+  sb-memory = pkgs.writeScriptBin "sb-memory" ''
+    case $BUTTON in
+      1) ${pkgs.libnotify}/bin/notify-send "Memory hogs" "$(${pkgs.procps}/bin/ps axch -o cmd:15,%mem --sort=-%mem | ${pkgs.coreutils}/bin/head)"
+        ;;
+      2) ${pkgs.alacritty}/bin/alacritty -e ${pkgs.htop}/bin/htop --sort PERCENT_MEM
+        ;;
       3) ${pkgs.libnotify}/bin/notify-send "Memory module" "\- Shows Memory Used/Total.
           - Click to show memory hogs.
-          - Middle click to open htop." ;;
-      6) "$TERMINAL" -e "$EDITOR" "$0" ;;
+          - Middle click to open htop."
+        ;;
     esac
-    ${pkgs.procps}/bin/free --mebi | sed -n '2{p;q}' | ${pkgs.gawk}/bin/awk '{printf ("%2.1f/%2.1fGiB\n", ( $3 / 1024), ($2 / 1024))}'
+    ${pkgs.procps}/bin/free | sed -n '2{p;q}' | ${pkgs.gawk}/bin/awk '{printf ("%.0f%", $3/$2 * 100.0)}'
   '';
-  my-dwmblocks = (pkgs.dwmblocks.override {
+  sb-fs = pkgs.writeScriptBin "sb-fs" ''
+    ${pkgs.coreutils}/bin/df ~/ | ${pkgs.coreutils}/bin/tail -n 1 | ${pkgs.gawk}/bin/awk -F ' +' '{print $5}'
+  '';
+  sb-date = pkgs.writeScriptBin "sb-date" ''
+    case $BUTTON in
+      1) ${pkgs.firefox}/bin/firefox --new-window "calendar.google.com"
+        ;;
+      2) ${pkgs.alacritty}/bin/alacritty -e ${pkgs.htop}/bin/htop
+        ;;
+      3) ${pkgs.coreutils}/bin/date -u +"%Y-%m-%d" | ${pkgs.coreutils}/bin/tr -d "\n" | ${pkgs.xclip}/bin/xclip -sel clip && ${pkgs.libnotify}/bin/notify-send "Copied date to clipboard"
+        ;;
+    esac
+    ${pkgs.coreutils}/bin/date +'%h %d (%a) %H:%M'
+  '';
+  dwmblocks = (pkgs.dwmblocks.override {
     conf = ''
       static const Block blocks[] = {
-        /*Icon*/  /*Command*/   /*Update Interval*/   /*Update Signal*/
-        {"Mem: ", "${memory}/bin/sb-memory",  30,     14},
-        {"",     "date +'%h %d (%a) %H:%M'",  60,     1},
+        /*Icon*/  /*Command*/     /*Update Interval*/   /*Update Signal*/
+        {"RAM: ", "${sb-memory}/bin/sb-memory",  30,     14},
+        {"/: ",   "${sb-fs}/bin/sb-fs",          60,     15},
+        {"",      "${sb-audio}/bin/sb-audio",    30,     16},
+        {"",      "${sb-date}/bin/sb-date",      60,      1},
       };
 
-      static char *delim = " | ";
+      static char delim[] = " | ";
+      static unsigned int delimLen = 5;
     '';
   });
 in {
@@ -34,7 +60,6 @@ in {
     # modules.theme.onReload.dwm = '''';
 
     environment.systemPackages = with pkgs; [ dunst libnotify ];
-    user.packages = [ memory ];
 
     services = {
       picom.enable = true;
@@ -67,8 +92,7 @@ in {
       wantedBy = [ "default.target" ];
       serviceConfig.Restart = "always";
       serviceConfig.RestartSec = 2;
-      serviceConfig.ExecStart = "${my-dwmblocks}/bin/dwmblocks";
-      path = [ memory ];
+      serviceConfig.ExecStart = "${dwmblocks}/bin/dwmblocks";
     };
 
     systemd.user.services."dunst" = {
--- overlays/dwm/default.nix
+++ overlays/dwm/default.nix
@@ -12,25 +12,18 @@ final: prev: {
       cp ${./patches.h} patches.def.h
     '';
     preBuild = ''
-      makeFlagsArray+=(XRENDER="-lXrender") # BAR_ALPHA_PATCH
+      makeFlagsArray+=(XRENDER="-lXrender")                 # BAR_ALPHA_PATCH
       makeFlagsArray+=(XCBLIBS="-lX11-xcb -lxcb -lxcb-res") # SWALLOW_PATCH
     '';
   });
 
   dwmblocks = prev.dwmblocks.overrideAttrs (oldAttrs: rec {
-    src = prev.fetchFromGitHub {
-      owner = "LukeSmithxyz";
-      repo = "dwmblocks";
-      rev = "66f31c307adbdcc2505239260ecda24a49eea7af";
-      sha256 = "sha256-j3wCRyl1+0D2XcdqhE5Zgf53bEXhcaU4dvdyYG9LZ2g=";
-    };
-    buildInputs = oldAttrs.buildInputs
-      ++ [ prev.xorg.libXft prev.xorg.libXinerama ];
-    prePatch = ''
-      sed -i "s@/usr/local/@$out@" Makefile
-    '';
-    preBuild = ''
-      mv blocks.def.h config.h
-    '';
+    patches = oldAttrs.patches ++ [
+      (final.fetchpatch {
+        url =
+          "https://dwm.suckless.org/patches/statuscmd/dwmblocks-statuscmd-20210402-96cbb45.diff";
+        sha256 = "sha256-ADFjvViuTOnY4pMYwVypUPW71CzrLUDIzXr/htldEvk=";
+      })
+    ];
   });
 }
