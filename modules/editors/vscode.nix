e1835854556eaf42c849578662f212e2ed320604
AntonHakansson <anton.hakansson98@gmail.com>
2021-09-06 02:09:02 +0200

    vscode: one dark theme

--- modules/editors/vscode.nix
+++ modules/editors/vscode.nix
@@ -2,21 +2,41 @@
 
 with lib;
 with lib.my;
-let cfg = config.modules.editors.vscode;
+let
+  cfg = config.modules.editors.vscode;
+  extensions = with pkgs.vscode-extensions;
+    [
+      vscodevim.vim
+      ms-vsliveshare.vsliveshare
+
+      # nix
+      bbenoist.nix
+
+      # typescript
+      jpoissonnier.vscode-styled-components
+    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
+      # One Dark Pro
+      name = "Material-theme";
+      publisher = "zhuangtongfa";
+      version = "3.11.4";
+      sha256 = "sha256-3jo5oToo3Lk94Z5VW/zWCWv1GcCUTC3F4fI+erD0SDI=";
+    }];
+  vscode-with-extensions = (pkgs.unstable.vscode-with-extensions.override {
+    vscodeExtensions = extensions;
+  });
 in {
-  options.modules.editors.vscode = {
-    enable = mkBoolOpt false;
-  };
+
+  options.modules.editors.vscode = { enable = mkBoolOpt false; };
 
   config = mkIf cfg.enable {
-    user.packages = with pkgs; [
-      (unstable.vscode-with-extensions.override {
-        vscodeExtensions = with vscode-extensions; [
-          bbenoist.nix
-          vscodevim.vim
-          ms-vsliveshare.vsliveshare
-        ];
-      })
-    ];
+    user.packages = [ vscode-with-extensions ];
+    home.configFile = {
+      "Code/User/settings.json".text = ''
+        {
+          "window.menuBarVisibility": "toggle"
+          "workbench.colorTheme": "One Dark Pro"
+        }
+      '';
+    };
   };
 }
