8ca9ed7a7873143d2175bdc53e31a1cb80dfb204
AntonHakansson <anton.hakansson98@gmail.com>
2021-09-08 14:54:48 +0200

    vscode: merge nixos & local settings
    home-manager uses a non-writable filesystem but vscode needs write
    access to update the settings so therefore we merge our nixos config
    with the user's local configuration

--- modules/editors/vscode.nix
+++ modules/editors/vscode.nix
@@ -33,14 +33,28 @@ in {
 
   config = mkIf cfg.enable {
     user.packages = [ vscode-with-extensions ];
-    home.configFile = {
-      "Code/User/settings.json".text = ''
-        {
-          "editor.fontFamily": "Fira Code",
-          "window.menuBarVisibility": "toggle",
-          "workbench.colorTheme": "One Dark Pro",
-          "vim.useSystemClipboard": true,
-        }
+    home.configFile."Code/User/nixos-settings.json".text = ''
+      {
+        "editor.fontFamily": "Fira Code",
+        "window.menuBarVisibility": "toggle",
+        "workbench.colorTheme": "One Dark Pro",
+        "vim.useSystemClipboard": true
+      }
+    '';
+
+    system.userActivationScripts = mkIf cfg.enable {
+      vscodeMergeConfigs = let
+        nixos-config = "$XDG_CONFIG_HOME/Code/User/nixos-settings.json";
+        code-config = "$XDG_CONFIG_HOME/Code/User/settings.json";
+      in ''
+        if [ -e ${code-config} ]
+        then
+          merged_settings=$(${pkgs.jq}/bin/jq -s '.[0] * .[1]' ${nixos-config} ${code-config})
+          $(echo $merged_settings | ${pkgs.jq}/bin/jq) && $(echo $merged_settings > ${code-config})
+        else
+          cp ${nixos-config} ${code-config}
+          chmod +w ${code-config}
+        fi
       '';
     };
   };
